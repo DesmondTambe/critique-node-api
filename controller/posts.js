53e841ecc48ead8a9399f5a3c6947a59e9fe2284
DesmondTambe <desking2000@gmail.com>
2021-10-29 04:59:52 +0100

    Wordpress headless api with node, fetching posts and categories

--- controller/posts.js
+++ controller/posts.js
@@ -3,16 +3,19 @@ const baseURL = require("../utils/baseURL");
 
 module.exports = {
   fetchPostsWP: async (req, res) => {
+    const { catId } = req.query;
     try {
       // fetch post from wordpress
       const posts = await axios.get(
-        `${baseURL}wp/v2/posts/?categories=681150187&page=1&per_page=10&_fields=id,date,title,content,custom,link,categories,jetpack_featured_media_url`
-        // &_fields=id,date,title,content,custom,link,categories`
+        `${baseURL}wp/v2/posts/?categories=${catId}&page=1&per_page=10&_fields=id,date,title,content,custom,link,categories,jetpack_featured_media_url`
       );
 
       res.status(200).json(posts.data);
     } catch (error) {
       console.log("Post fecth error:", error);
+      res
+        .status(error.port)
+        .send("Failed to fetch data, Please check your internet connection.");
     }
   },
 };
